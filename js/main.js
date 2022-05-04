3a2a4494be990c5c161caa36c1a2aae04417f7eb
Mohamed Ashraf <botoashraf123@gmail.com>
2022-05-04 20:28:54 +0200

    add preloader animation

--- js/main.js
+++ js/main.js
@@ -12,15 +12,24 @@ document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
     document.querySelector('#navbarNav').classList.remove('show')
   })
 })
+
 //stop propagation on menu area
 document.querySelector('.navbar-collapse .navbar-nav').addEventListener('click', function(e) {
   e.stopPropagation()
 });
 
+// add bg to header
 window.onscroll = function() {
   if(this.scrollY > 20) {
     document.querySelector('header').classList.add('sticky')
   } else {
     document.querySelector('header').classList.remove('sticky')
   }
+}
+
+// hide preloader
+window.onload = function () { 
+  var s = document.querySelector('.preloader').style;
+  s.opacity = 1;
+  (function fade(){(s.opacity-=.05) < 0 ? s.display="none" :setTimeout(fade,40)})();
 }
